7fe9c2bb4356f42696c86f59ba56c3b757cf75f8
Shivkp92 <vs.shivkumar@gmail.com>
2017-09-18 10:43:12 -0400

    New files yo

---
+++ indexx.cpp
@@ -0,0 +1,35 @@
+/*
+ * indexx.cpp
+ *
+ *  Created on: Sep 18, 2017
+ *      Author: SL
+ */
+
+#ifndef INDEXX_CPP_
+#define INDEXX_CPP_
+
+
+
+
+
+#endif /* INDEXX_CPP_ */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{ int a,b,c,t,i;
+	a=2;
+	b=3;
+	c=a*b;
+	t=0;
+	for (int i=1;i<6;i++)
+		{t=t+c;
+		};
+
+	cout<<t;
+
+	return 0;
+
+		}
